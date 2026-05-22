0635d872f93a7804aa5b43dc4d86f0da8c66a9bd
QIZHAO333 <105961937@student.swin.edu.au>
2026-05-23 00:42:40 +1000

    Update useUser.js

--- src/views/User/composables/useUser.js
+++ src/views/User/composables/useUser.js
@@ -1,13 +1,36 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
+import api from '../../../services/api.js'
+
 const user = ref(null)
+
 export function useUser() {
   const loadUser = () => {
     const stored = localStorage.getItem('user')
-    if (stored) user.value = JSON.parse(stored)
+    if (stored) {
+      user.value = JSON.parse(stored)
+      fetchLatestUser() 
+    }
   }
+
+  const fetchLatestUser = async () => {
+    if (!user.value || !user.value.id) return
+
+    try {
+      const res = await api.get('/user', {
+        params: { id: user.value.id }
+      })
+
+      user.value = res.data
+      localStorage.setItem('user', JSON.stringify(res.data))
+    } catch (err) {
+      console.log('Use local cache')
+    }
+  }
+
   const logout = () => {
     localStorage.removeItem('user')
     user.value = null
   }
+
   return { user, loadUser, logout }
 }
