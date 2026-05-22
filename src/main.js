3b45d297dca47853d402a4d6fcb08f30203de7d3
QIZHAO333 <105961937@student.swin.edu.au>
2026-05-22 15:14:18 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useUser } from './views/User/composables/useUser'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
