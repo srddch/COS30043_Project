aa39201e3d7e67e14409838192fd7e5b15607d23
Zhenghao Sun <105965706@student.eud.au>
2026-05-30 16:02:51 +1000

    hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 //成员5
 import MyLikes from '../views/Social/MyLikes.vue'
